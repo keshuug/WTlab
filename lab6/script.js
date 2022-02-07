1291c890e16e874e86b4f0ece0c6312f810dc459
keshuug <kartikgoswami082@gmail.com>
2022-02-07 17:33:13 +0530

    commit

---
+++ lab6/script.js
@@ -0,0 +1,15 @@
+//  A function to print Hello World.
+function msg(message){
+    return message;
+}
+//  A function that takes two numbers as inputs and return the sum as output.
+function sum(num1,num2){
+    return num1+num2;
+}
+// A function that performs basic calculator operations.
+function calculator(num1,num2){
+    console.log("Addition : "+num1+num2);
+    console.log("Subtraction : "+(num1-num2));
+    console.log("Multiplication : "+num1*num2);
+    console.log("Division : "+num1/num2);
+}
